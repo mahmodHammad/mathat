b6bb6d9bebf5991d23199c9d25b8c1e95a96b3ae
= <hodaman2012@gmail.com>
2021-09-03 21:20:52 +0200

    putting textrue as a map

--- three/sceneItems.js
+++ three/sceneItems.js
@@ -4,7 +4,10 @@ import { loadModel } from "./ModelLoader.js";
 import {setHDRLighting} from "./panorama.js"
 
 const  wheel = "three/models/test.glb"
+const txturl = "three/texture.png"
 let model= undefined
+
+var textureLoader = new THREE.TextureLoader();
 
 function addLights() {
   const amplight = new THREE.AmbientLight("#ffffff", 0.1);
@@ -28,7 +31,24 @@ const addToScene = () => {
 
   loadModel(wheel).then(glb=>{
     console.log(glb)
-    // const l1 = glb.getObjectByName
+    const l1 = glb.getObjectByName("Circle001_3")
+    const l2 = glb.getObjectByName("Circle005_1")
+    const l3 = glb.getObjectByName("Circle003_5")
+    console.log("HEY l2",l2)
+    console.log("HEY l2",l3)
+  const target = l1.children[0].children[0]
+  // target.visable= false
+    setTimeout(() => {
+      // target.material.color = new THREE.Color(0x00ff00)
+      const boxTexture = textureLoader.load("https://threejsfundamentals.org/threejs/resources/images/wall.jpg",(t)=>{
+        target.material.map = t
+        console.log("TTT",t)
+      }  )
+      console.log("HEY LOL",target)
+      
+      console.log("LOADED",boxTexture)
+    }, 100);
+
     // glb.position.set(0, 0, -2)
     glb.scale.set(0.1,0.1,0.1)
     scene.add(glb)
